20c30588a8f2b676a22efbae61ab766e55e38ef8
MikeHibbert <hibbert.michael@gmail.com>
2020-10-02 05:23:39 +0100

    updated to use EMD repo

--- owncloud/owncloud-client/owncloud-client.py
+++ owncloud/owncloud-client/owncloud-client.py
@@ -16,7 +16,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues(tarballUrl="https://download.owncloud.com/desktop/stable/owncloudclient-${VERSION}.tar.xz",
                                           tarballInstallSrc="owncloudclient-${VERSION}",
-                                          gitUrl="[git]https://github.com/owncloud/client")
+                                          gitUrl="[git]https://github.com/MikeHibbert/owncloud-client")
 
         # we don't have that branche yet
         self.svnTargets["2.8"] = self.svnTargets["master"]
